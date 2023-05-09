8f25c412b0c6aa5ea025b9cbb57d8a50f94da653
RaghavendranG <raghavendrang7@gmail.com>
2023-05-09 14:26:06 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
-import HomePage from './components/Homepage';
-import { Route, Routes } from 'react-router-dom';
-import EditFile from './components/EditFile';
-import ViewCommits from './components/ViewCommits';
-import CreateRepository from './components/CreateRepository';
-import LogIn from './components/Login';
-
-
-
+import "./App.css";
+import HomePage from "./components/Homepage";
+import { Route, Routes } from "react-router-dom";
+import EditFile from "./components/EditFile";
+import ViewCommits from "./components/ViewCommits";
+import CreateRepository from "./components/CreateRepository";
+import LogIn from "./components/Login";
 
 function App() {
-
   return (
-    
     <Routes>
-      <Route path='/'element={<LogIn/>}/>
-      <Route path='/homepage' element={<HomePage />}/>
-      <Route path='/editFile/:id' element={<EditFile/>}/>
-      <Route path='/commits/:id'element={<ViewCommits/>}/>
-      <Route path='/create'element={<CreateRepository/>}/>
+      <Route path="/" element={<LogIn />} />
+      <Route path="/homepage" element={<HomePage />} />
+      <Route path="/editFile/:id" element={<EditFile />} />
+      <Route path="/commits/:id" element={<ViewCommits />} />
+      <Route path="/create" element={<CreateRepository />} />
     </Routes>
   );
 }
